897ed7b0779d66c4e316df794716a270c1b45b76
Dragan Silviu <silviu.dragan@gmail.com>
2017-04-09 15:59:54 +0300

    asda

--- register.php
+++ register.php
@@ -1,9 +1,10 @@
 <!DOCTYPE html>
+
 <head>
 	<meta charset="utf-8">
 	<meta http-equiv="X-UA-Compatible" content="IE=edge">
 	<meta name="viewport" content="width=device-width, initial-scale=1">
-	<title>Update</title>
+	<title>Register</title>
 	<link href="css/bootstrap.min.css" rel="stylesheet">
 	<link href="css/font-awesome.min.css" rel="stylesheet">
 	<link href="css/home-page.css" rel="stylesheet">
@@ -14,45 +15,47 @@
 
 	<div class="navbar-collapse collapse" style="background-color:#d6d6c2; margin-top: 0px;">
 		<ul class="nav navbar-nav navbar-left" style="margin-left: 50px;">
-			<li><a href="meniu.php?user=<?php echo $username; ?>"> Home </a></li>
-			<li><a href="profile.php?user=<?php echo $username; ?>">Profile</a></li>
-			<li><a href="test_connection.php">Logout</a></li>
+			<li><a href="test_connection.php"> Home </a></li>
+			<li><a href="login.php">Login</a></li>
+			<li><a href="register.php">Register</a></li>
 		</ul>
 	</div>
+
 
 	<?php
 		require_once(dirname(__FILE__) . '/functions.php');
 		$connection = connect();
 		// define variables and set to empty values
-		$usernameErr = $newpasswordErr = $passwordErr = $newpassword2Err = "";
-		$username = $newpassword = $password = $newpassword2 = "";
+		$usernameErr = $emailErr = $passwordErr = $repasswordErr = "";
+		$username = $email = $password = $repassword = "";
 
 		if ($_SERVER["REQUEST_METHOD"] == "POST") {
 		  if (empty($_POST["username"])) {
-		    $usernameErr = "Username is required";
+		    $usernameErr = "Name is required";
 		  } else {
 		    $username = test_input($_POST["username"]);
 		  }
-
+		  
 		  if (empty($_POST["password"])) {
-		    $passwordErr = "Old password is required";
+		    $passwordErr = "Password is required";
 		  } else {
 		    $password = test_input($_POST["password"]);
 		  }
-		  
-		  if (empty($_POST["newpassword"])) {
-		    $newpasswordErr = "New password is required";
+		    
+		  if (empty($_POST["repassword"])) {
+		    $repasswordErr = "Confirm password is required";
 		  } else {
-		    $newpassword = test_input($_POST["newpassword"]);
+		    $repassword = test_input($_POST["repassword"]);
 		  }
 
-		  if (empty($_POST["newpassword2"])) {
-		    $newpassword2Err = "Confirm password is required";
+		  if (empty($_POST["email"])) {
+		    $email = "";
 		  } else {
-		    $newpassword2 = test_input($_POST["newpassword2"]);
+		    $email = test_input($_POST["email"]);
 		  }
 
 		}
+
 		function test_input($data) {
 		  $data = trim($data);
 		  $data = stripslashes($data);
@@ -61,51 +64,84 @@
 		}
 	?>
 
-	<h2 style="text-align: center;">Change password</h2>
+	<h2 style="text-align: center;">Register</h2>
+	<p style="text-align: center;"><span class="error">* required field.</span></p>
 	<form method="post" action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]);?>" style="text-align: center;">  
 	  Username: <input type="text" name="username">
-	  <span class="error"><?php echo $usernameErr;?></span>
+	  <span class="error">* <?php echo $usernameErr;?></span>
 	  <br><br>
-	  Old password: <input type="password" name="password">
-	  <span class="error"><?php echo $passwordErr;?></span>
+	  Password: <input type="password" name="password">
+	  <span class="error">* <?php echo $passwordErr;?></span>
 	  <br><br>
-	  New password: <input type="password" name="newpassword">
-	  <span class="error"><?php echo $newpasswordErr;?></span>
+	  Confirm password: <input type="password" name="repassword">
+	  <span class="error">* <?php echo $repasswordErr;?></span>
 	  <br><br>
-	  Confirm password: <input type="password" name="newpassword2">
-	  <span class="error"><?php echo $newpassword2Err;?></span>
+	  E-mail: <input type="text" name="email">
+	  <span class="error"><?php echo $emailErr;?></span>
 	  <br><br>
-	  <input type="submit" name="submit" value="Change password">
-	  <input type="reset"> 
+	  <input type="submit" name="submit" value="Submit">  
 	</form>
 
 	<?php
-		// Prepare the statement
-		if ($_SERVER["REQUEST_METHOD"] == "POST"){
-			$query = "UPDATE users set password='$newpassword' where username='$username'";
-			$stid = oci_parse($connection, $query);
-			if (!$stid) {
-			    $e = oci_error($connection);
-			    trigger_error(htmlentities($e['message'], ENT_QUOTES), E_USER_ERROR);
-			}
-
-			// Perform the logic of the query
-			$r = oci_execute($stid);
-			if (!$r) {
-			    $e = oci_error($stid);
-			    trigger_error(htmlentities($e['message'], ENT_QUOTES), E_USER_ERROR);
-			}
-			else{
+		if($password != $repassword)
+			{
 				echo "<script type = \"text/javascript\">
-	                                    alert(\"Password changed...\");
-	                                    window.location = (\"meniu.php?user=$username\");
+	                                    alert(\"Passwords are different...\");
+	                                    window.location = (\"register.php\");
 	                                    </script>"; 
+	        }
+	      else{
+			// Prepare the statement
+			if ($_SERVER["REQUEST_METHOD"] == "POST"){
+
+				$query = "SELECT max(id) from USERS";
+				$stid = oci_parse($connection, $query);
+				if (!$stid) {
+				    $e = oci_error($connection);
+				    trigger_error(htmlentities($e['message'], ENT_QUOTES), E_USER_ERROR);
+				}
+
+				// Perform the logic of the query
+				$r = oci_execute($stid);
+				if (!$r) {
+				    $e = oci_error($stid);
+				    trigger_error(htmlentities($e['message'], ENT_QUOTES), E_USER_ERROR);
+				}
+
+				// Fetch the result of the query
+				$max_id = 0;
+				while ($row = oci_fetch_array($stid, OCI_ASSOC+OCI_RETURN_NULLS)) {
+				    foreach ($row as $item) {
+			        $max_id = $item;
+			    	}
+				}
+				$max_id++;
+
+				$query = "INSERT INTO users values($max_id, 1, '$username', '$password', 0, 0, sysdate)";
+				$stid = oci_parse($connection, $query);
+				if (!$stid) {
+				    $e = oci_error($connection);
+				    trigger_error(htmlentities($e['message'], ENT_QUOTES), E_USER_ERROR);
+				}
+
+				// Perform the logic of the query
+				$r = oci_execute($stid);
+				if (!$r) {
+				    echo "<script type = \"text/javascript\">
+	                                    alert(\"* fields must be completed\");
+	                                    window.location = (\"register.php\");
+	                                    </script>"; 
+				}
+				else{
+					echo "<br> <h4 style='text-align: center'> Registration successful </h4>";
+		            echo "<br><a href='meniu.php?user=$username'><h4 style='text-align: center'> Click to access the main page </h4></a>";
+				}
+
+
+				oci_free_statement($stid);
+				}
 			}
-			oci_free_statement($stid);
-		}
-
 	?>
-
 
 </body>
 
