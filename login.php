a1e518f6e5fed11965866aeb7213d28b9c1dd8cf
Dragan Silviu <silviu.dragan@gmail.com>
2017-04-09 16:00:35 +0300

    mai avem doar 3 chestii de facut din lista

--- login.php
+++ login.php
@@ -1,10 +1,9 @@
 <!DOCTYPE html>
-
 <head>
 	<meta charset="utf-8">
 	<meta http-equiv="X-UA-Compatible" content="IE=edge">
 	<meta name="viewport" content="width=device-width, initial-scale=1">
-	<title>Welcome page</title>
+	<title>Login</title>
 	<link href="css/bootstrap.min.css" rel="stylesheet">
 	<link href="css/font-awesome.min.css" rel="stylesheet">
 	<link href="css/home-page.css" rel="stylesheet">
@@ -21,52 +20,90 @@
 		</ul>
 	</div>
 
-
-
-
 	<?php
-
 		require_once(dirname(__FILE__) . '/functions.php');
 		$connection = connect();
+		// define variables and set to empty values
+		$usernameErr = $passwordErr = "";
+		$username = $password = "";
 
+		if ($_SERVER["REQUEST_METHOD"] == "POST") {
+		  if (empty($_POST["username"])) {
+		    $usernameErr = "Username is required";
+		  } else {
+		    $username = test_input($_POST["username"]);
+		  }
+		  
+		  if (empty($_POST["password"])) {
+		    $passwordErr = "Password is required";
+		  } else {
+		    $password = test_input($_POST["password"]);
+		  }
+
+		}
+
+		function test_input($data) {
+		  $data = trim($data);
+		  $data = stripslashes($data);
+		  $data = htmlspecialchars($data);
+		  return $data;
+		}
 	?>
 
-	<h3 style="text-align: center;">Top 10 music voted</h3>
+	<h2 style="text-align: center;">Login</h2>
+	<form method="post" action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]);?>" style="text-align: center;">  
+	  Username: <input type="text" name="username">
+	  <span class="error"><?php echo $usernameErr;?></span>
+	  <br><br>
+	  Password: <input type="password" name="password">
+	  <span class="error"><?php echo $passwordErr;?></span>
+	  <br><br>
+	  <input type="submit" name="submit" value="Submit">
+	  <input type="reset"> 
+	</form>
+
 	<?php
 		// Prepare the statement
-		$stid = oci_parse($connection, 'SELECT * from (SELECT rank()over(order by votes desc) as pozitie, id_song, name, link, votes, posted_time from SONGS) where pozitie <= 10');
-		if (!$stid) {
-		    $e = oci_error($connection);
-		    trigger_error(htmlentities($e['message'], ENT_QUOTES), E_USER_ERROR);
-		}
+		if ($_SERVER["REQUEST_METHOD"] == "POST"){
 
-		// Perform the logic of the query
-		$r = oci_execute($stid);
-		if (!$r) {
-		    $e = oci_error($stid);
-		    trigger_error(htmlentities($e['message'], ENT_QUOTES), E_USER_ERROR);
-		}
+			$query = "SELECT * from USERS where USERNAME='$username' and PASSWORD='$password'";
+			$stid = oci_parse($connection, $query);
+			if (!$stid) {
+			    $e = oci_error($connection);
+			    trigger_error(htmlentities($e['message'], ENT_QUOTES), E_USER_ERROR);
+			}
 
-		// Fetch the results of the query
-		print "<table border='1' align='center'>\n";
-		print "<tr>\n";
-		print "<td>Rank</td><td> ID Song </td><td> Song </td><td> Link </td><td> Votes </td><td> Published Date </td>";
-		print "</tr>";
-		while ($row = oci_fetch_array($stid, OCI_ASSOC+OCI_RETURN_NULLS)) {
-		    print "<tr>\n";
-		    foreach ($row as $item) {
-		        print "    <td>" . ($item !== null ? htmlentities($item, ENT_QUOTES) : "&nbsp;") . "</td>\n";
-		    }
-		    print "</tr>\n";
-		}
-		print "</table>\n";
+			// Perform the logic of the query
+			$r = oci_execute($stid);
+			if (!$r) {
+			    $e = oci_error($stid);
+			    trigger_error(htmlentities($e['message'], ENT_QUOTES), E_USER_ERROR);
+			}
 
-		oci_free_statement($stid);
-		// Close connection 
-		//oci_close($connection);
+			// Fetch the results of the query
+			$valid = false;
+			while ($row = oci_fetch_array($stid, OCI_ASSOC+OCI_RETURN_NULLS)) {
+			    $valid = true;
+			}
+			if($valid == false){
+				 echo "<script type = \"text/javascript\">
+	                                    alert(\"Login failed...\");
+	                                    window.location = (\"login.php\");
+	                                    </script>"; 
+			}
+			else{
+	            echo "<br><a href='meniu.php?user=$username'><h4 style='text-align: center'> Click to access the main page </h4></a>";
+			}
+
+
+			oci_free_statement($stid);
+		}
 
 	?>
-	</body>
+
+
+</body>
+
 
 
 </html>
