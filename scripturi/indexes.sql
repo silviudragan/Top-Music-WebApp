4fd1837d63fbaef6a2430dfa362d420a3be7e466
Razvan Romanescu <romanescu.raz@gmail.com>
2017-05-29 22:16:19 +0300

    Am adaugat partea de indecsi si view materializat

---
+++ scripturi/indexes.sql
@@ -0,0 +1,29 @@
+set serveroutput on;
+
+-- First index on ORDER BY clause
+drop index first_index;
+
+create index first_index
+on songs(votes DESC, posted_time DESC);
+
+select * from songs where posted_time >= TO_DATE('01-JAN-88', 'dd-mm-yyyy') order by votes DESC, posted_time DESC;
+
+drop index second_index;
+
+select id_comm, id_user, comm from comments where length(comm) > 5 group by id_comm, id_user, comm;
+
+-- Second index on GROUP BY clause
+drop index second_index;
+
+create index second_index
+on songs(id_user, votes);
+
+select id_user, votes from songs where id_user > 100 group by id_user, votes;
+
+-- Third index on JOIN clause
+drop index third_index;
+
+create index third_index
+on comments(comm, posted_time);
+
+select comm, posted_time from comments c, users u where c.id_user = u.id;
---
+++ scripturi/view.sql
@@ -0,0 +1,24 @@
+--CREATE MATERIALIZED VIEW users_comments AS
+--  select u.id, comm, posted_time from comments c, users u 
+--  where c.id_user = u.id and length(comm) > 5 and posted_time >= TO_DATE('01-10-2015', 'dd-mm-yyyy');
+--
+--DROP MATERIALIZED VIEW users_comments;
+  
+CREATE MATERIALIZED VIEW comments_view FOR UPDATE AS
+  select id_comm, id_user, comm, posted_time from comments c 
+  where length(comm) > 5 and posted_time >= TO_DATE('01-10-2015', 'dd-mm-yyyy')
+  order by posted_time desc;
+
+DROP MATERIALIZED VIEW comments_view;
+
+SELECT * FROM comments_view;
+select * from comments_view where id_comm = 3000;
+select max(id_comm) from comments_view;
+
+INSERT INTO comments_view VALUES(3000, 177, 'Tralalala', SYSDATE);
+
+INSERT INTO comments_view VALUES(3001, 177, 'Meow', SYSDATE);
+UPDATE comments_view SET comm = 'Meow-Meow' WHERE id_comm = 3001;
+
+DELETE FROM comments_view WHERE id_comm = 3000;
+DELETE FROM comments_view WHERE id_comm = 3001;
